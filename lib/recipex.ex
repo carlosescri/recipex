c7369326583e6612e29b454c9913e56d434d7a98
Carlos Escribano <carlos@nettoys.es>
2023-04-12 22:33:10 +0200

    add auto-gen specs

--- lib/recipex.ex
+++ lib/recipex.ex
@@ -6,6 +6,8 @@ defmodule Recipex do
   alias Recipex.Parser
   alias Recipex.Recipe
 
+  @spec parse(binary, any) ::
+          {:ok, list, binary, map, {pos_integer, pos_integer}, pos_integer} | map
   def parse(text, opts \\ []) do
     with {:ok, [], "", recipe, _, _} <- Parser.parse_recipe(text) do
       if Keyword.get(opts, :reduce, false) do
@@ -16,6 +18,14 @@ defmodule Recipex do
     end
   end
 
+  @spec parse_file(
+          binary
+          | maybe_improper_list(
+              binary | maybe_improper_list(any, binary | []) | char,
+              binary | []
+            ),
+          any
+        ) :: {:ok, list, binary, map, {pos_integer, pos_integer}, pos_integer} | map
   def parse_file(path, opts \\ []) do
     path |> File.read!() |> parse(opts)
   end
